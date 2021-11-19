d8749b00f72b64924b099c824de533009b2359f2
xplanet95 <vladis.box@yandex.ru>
2021-11-19 15:12:54 +0800

    --

--- web-p/HW/03bs4/parse.py
+++ web-p/HW/03bs4/parse.py
@@ -1,25 +1,28 @@
 from bs4 import BeautifulSoup as bs
 import unittest
 import os
+import re
 
 
 
 def parse(path_to_file):
     with open(path_to_file, 'r', encoding='utf-8') as html:
         soup = bs(html, 'lxml')
-        print([soup.a.next for i in soup])
+        imgs = len(soup.find_all('img',  width=lambda x: int(x or 0) > 199))
+        pattern = re.compile(r'[hH1-6]{2}')
+        headers = len([i.text for i in soup.find_all(name=pattern) if i.text[0] in 'ETC']) - 1
 
-    return [imgs, headers, linkslen, lists]
+    # return [imgs, headers, linkslen, lists]
 
 
 class TestParse(unittest.TestCase):
     def test_parse(self):
         test_cases = (
-            ('wiki/Stone_Age', [13, 10, 12, 40]),
-            ('wiki/Brain', [19, 5, 25, 11]),
             ('wiki/Artificial_intelligence', [8, 19, 13, 198]),
+            ('wiki/Brain', [19, 5, 25, 11]),
             ('wiki/Python_(programming_language)', [2, 5, 17, 41]),
-            ('wiki/Spectrogram', [1, 2, 4, 7]),)
+            ('wiki/Spectrogram', [1, 2, 4, 7]),
+            ('wiki/Stone_Age', [13, 10, 12, 40]),)
 
         for path, expected in test_cases:
             with self.subTest(path=path, expected=expected):
